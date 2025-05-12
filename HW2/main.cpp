3741c6bd2d0d4f73e7a96e864c9e8c9ec9b78219
tonychu27 <tony20011027@gmail.com>
2025-05-13 03:04:21 +0800

    HW2; load, cont, si, disassemble

---
+++ HW2/main.cpp
@@ -0,0 +1,221 @@
+#include <iostream>
+#include <fstream>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <string.h>
+#include <elf.h>
+#include <capstone/capstone.h>
+
+#define errquit(x) {perror(x); exit(1);}
+
+using namespace std;
+
+pid_t child_pid = -1;
+
+bool loaded = false;
+bool non_static = false;
+
+uintptr_t base_address = 0;
+
+void disassemble(pid_t pid, uintptr_t rip) {
+    constexpr int INSN_BUF_SIZE = 64;
+    uint8_t code[INSN_BUF_SIZE];
+
+    for(int i = 0; i < INSN_BUF_SIZE; i += sizeof(long)) {
+        errno = 0;
+        long data = ptrace(PTRACE_PEEKDATA, pid, rip + i, nullptr);
+        if(errno != 0) errquit("ptrace(PEEKDATA)");
+        memcpy(code + i, &data, sizeof(long));
+    }
+
+    csh handle;
+    if(cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) errquit("Capstone Initialize Failed");
+    
+    cs_insn *insn;
+    size_t count = cs_disasm(handle, code, INSN_BUF_SIZE, rip, 5, &insn);
+    if(count > 0) {
+        for(size_t i = 0; i < count; i++) {
+            printf("        %6lx:  ", insn[i].address);
+            for (int j = 0; j < insn[i].size; j++) printf("%02x ", insn[i].bytes[j]);
+            int padding = (8 - insn[i].size) * 3;
+            for (int j = 0; j < padding; j++) printf(" ");
+            printf("%-7s %s\n", insn[i].mnemonic, insn[i].op_str);
+        }
+
+        cs_free(insn, count);
+    }
+    else printf("** the address is out of the range of the executable region.\n");
+
+    cs_close(&handle);
+
+}
+
+void load_program(const char* file_path) {
+    if(loaded) {
+        puts("Program has been loaded");
+        return;
+    }
+
+    child_pid = fork();
+
+    if(child_pid == 0) {
+        ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
+        execl(file_path, file_path, nullptr);
+        errquit("execl failed");
+    }
+    else if(child_pid > 0) {
+        int status;
+        waitpid(child_pid, &status, 0);
+
+        char abs_path[PATH_MAX];
+        realpath(file_path, abs_path);
+
+        char maps_path[64];
+        sprintf(maps_path, "/proc/%d/maps", child_pid);
+        FILE* maps = fopen(maps_path, "r");
+        if(!maps) errquit("fopen maps");
+
+        char line[256];
+        while(fgets(line, sizeof(line), maps)) {
+            if (strstr(line, "r--p") && strstr(line, abs_path)) {
+                sscanf(line, "%lx-", &base_address);
+                break;
+            }
+        }
+
+        fclose(maps);
+        if (base_address == 0) {
+            puts("** failed to find base address.");
+            return;
+        }
+
+        int fd = open(file_path, O_RDONLY);
+        if(fd < 0) errquit("open failed");
+
+        Elf64_Ehdr elf_header;
+        read(fd, &elf_header, sizeof(elf_header));
+        loaded = true;
+        close(fd);
+
+        struct user_regs_struct regs;
+        if(ptrace(PTRACE_GETREGS, child_pid, nullptr, &regs) < 0) errquit("ptrace(GETREGS)");
+
+        /*** handling non-static binary ***/
+        if(elf_header.e_entry != regs.rip) {
+            non_static = true;
+            long entry_point = base_address + elf_header.e_entry;
+            printf("** program '%s' loaded. entry point: 0x%lx.\n", file_path, entry_point);
+            disassemble(child_pid, entry_point);
+        }
+        else {
+            printf("** program '%s' loaded. entry point: 0x%lx.\n", file_path, (long)elf_header.e_entry);
+            disassemble(child_pid, regs.rip);
+        }
+    }
+    else errquit("fork failed");
+
+}
+
+void step_instruction() {
+    int status;
+
+    if(ptrace(PTRACE_SINGLESTEP, child_pid, nullptr, nullptr) < 0) errquit("ptrace(SINGLESTEP)");
+
+    waitpid(child_pid, &status, 0);
+
+    if(WIFEXITED(status)) {
+        puts("** the target program terminated.");
+        return;
+    }
+
+    struct user_regs_struct regs;
+    if(ptrace(PTRACE_GETREGS, child_pid, nullptr, &regs) < 0) errquit("ptrace(GETREGS)");
+
+    disassemble(child_pid, regs.rip);
+}
+
+void continue_execution() {
+    if(ptrace(PTRACE_CONT, child_pid, nullptr, nullptr) < 0) errquit("ptrace(CONT)");
+
+    int status;
+    waitpid(child_pid, &status, 0);
+
+    if(WIFEXITED(status)) {
+        puts("** the target program terminated.");
+        return;
+    }
+    if(WIFSTOPPED(status)) {
+        struct user_regs_struct regs;
+        if(ptrace(PTRACE_GETREGS, child_pid, nullptr, &regs) < 0) errquit("ptrace(GETREGS)");
+        printf("** program stopped at 0x%llx\n", regs.rip);
+        disassemble(child_pid, regs.rip);
+    }
+}
+
+void list_registers() {
+    struct user_regs_struct regs;
+    if(ptrace(PTRACE_GETREGS, child_pid, nullptr, &regs) < 0) errquit("ptrace(GETREGS)");
+
+    printf("$rax 0x%016llx    $rbx 0x%016llx    $rcx 0x%016llx\n", regs.rax, regs.rbx, regs.rcx);
+    printf("$rdx 0x%016llx    $rsi 0x%016llx    $rdi 0x%016llx\n", regs.rdx, regs.rsi, regs.rdi);
+    printf("$rbp 0x%016llx    $rsp 0x%016llx    $r8  0x%016llx\n", regs.rbp, regs.rsp, regs.r8);
+    printf("$r9  0x%016llx    $r10 0x%016llx    $r11 0x%016llx\n", regs.r9, regs.r10, regs.r11);
+    printf("$r12 0x%016llx    $r13 0x%016llx    $r14 0x%016llx\n", regs.r12, regs.r13, regs.r14);
+    printf("$r15 0x%016llx    $rip 0x%016llx    $eflags 0x%016llx\n", regs.r15, regs.rip, regs.eflags);
+}
+
+uintptr_t parse_address(const char *str) {
+    uintptr_t addr;
+    if(strncmp(str, "0x", 2) == 0) sscanf(str, "%lx", &addr);
+    else sscanf(str, "%lx", &addr);
+
+    return addr;
+}
+
+void handler(char* cmd) {
+    if(!loaded && strncmp(cmd, "load ", 5) != 0) {
+        cout << "** please load a program first.\n";
+        return;
+    }
+
+    if(!strcmp(cmd, "si\n")) step_instruction();
+    if(!strcmp(cmd, "cont\n")) continue_execution();
+    if(!strcmp(cmd, "info reg\n")) list_registers();
+
+    if(!strcmp(cmd, "quit\n") || !strcmp(cmd, "exit\n")) {
+        puts("Thanks for using my debugger. I hope you have a nice day!");
+        exit(0);
+    }
+    if(!strncmp(cmd, "load ", 5)) {
+        char *file_path = cmd + 5;
+        int length = 0;
+        while(file_path[length] != '\n') length++;
+        file_path[length] = '\0';
+        load_program(file_path);
+    }
+}
+
+int main(int argc, char** argv) {
+    if(argc == 2) load_program(argv[1]);
+    
+    while(true) {
+        char cmd[128];
+        cout << "(sdb) ";
+
+        if(fgets(cmd, sizeof(cmd), stdin) == nullptr) {
+            printf("\n");
+            continue;
+        }
+
+        handler(cmd);
+    }
+
+    return 0;
+}
---
+++ HW2/pkg_setup.sh
@@ -0,0 +1,2 @@
+apt update
+apt install -y sudo wget curl gcc make g++ man gdb tmux vim file libcapstone-dev expect
